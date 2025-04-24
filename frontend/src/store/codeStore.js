7a2e28dfb9f185ef6ac52b074f30ab1ce8b84b89
Priyansu <spriyansumeitei@gmail.com>
2025-04-24 21:08:40 +0530

    frontend api replacing for admin

--- frontend/src/store/codeStore.js
+++ frontend/src/store/codeStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 // const API_URL = import.meta.env.MODE === "development" ? "http://localhost:8080/api/auth" : "/api/auth";
-const API_URL = "http://localhost:8080/api/code";
+const API_URL = "http://localhost:8080/api";
 
 
 axios.defaults.withCredentials = true;
@@ -17,7 +17,7 @@ export const useCodeStore = create((set) => ({
 	// get all types 
 	fetchTypes: async () => {
 		try {
-			const response = await axios.get(`${API_URL}/types`);
+			const response = await axios.get(`${API_URL}/code/types`);
 			set({ types: response.data.types});
 		} catch (error) {
 			set({ error: error.response.data.message || "Error fetchTypes"});
@@ -25,20 +25,26 @@ export const useCodeStore = create((set) => ({
 		}
 	},
 
-	// get all variants with the corresponding type
+	// get all variants with the corresponding type (admin + user)
 	fetchVariants: async (id) => {
 		try {
-			const response = await axios.get(`${API_URL}/variants/?id=${id}`);
-			set({ types: response.data.type, variants: response.data.variants});
+			if (id == "admin") {
+				const response = await axios.get(`${API_URL}/admin/variants/?id=${id}`);
+				set({ types: response.data.type, variants: response.data.variants});
+			} else {
+				const response = await axios.get(`${API_URL}/code/variants/?id=${id}`);
+				set({ types: response.data.type, variants: response.data.variants});
+			}
 		} catch (error) {
 			set({ error: error.response.data.message || "Error fetchVariants"});
 			throw error;
 		}
 	},
 
+	// admin
 	priceUpdate: async (variant_id, priceINR, priceUSDT) => {
 		try {
-			const response = await axios.post(`${API_URL}/variants/priceUpdate`, {variant_id, priceINR, priceUSDT});
+			const response = await axios.post(`${API_URL}/admin/variants/priceUpdate`, {variant_id, priceINR, priceUSDT});
 			set({ message: response.data.message });
 		} catch (error) {
 			set({ error: error.response.data.message || "Error priceUpdate"});
